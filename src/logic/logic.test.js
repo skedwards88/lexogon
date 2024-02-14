d6f4dc17cb8d50c8c3657387741e5c6b6eadd434
skedwards88 <skedwards88@github.com>
2024-02-14 08:03:18 -0800

    prettier

--- src/logic/logic.test.js
+++ src/logic/logic.test.js
@@ -1,12 +1,9 @@
-import { getClue } from "./logic";
+import {getClue} from "./logic";
 
 describe("getClue", () => {
-
   test("todo", () => {
-
     expect(1).toEqual(1);
-    const clue = getClue({color: "purple", level: 5, test: false})
-    console.log(clue)
+    const clue = getClue({color: "purple", level: 5, test: false});
+    console.log(clue);
   });
-
 });
